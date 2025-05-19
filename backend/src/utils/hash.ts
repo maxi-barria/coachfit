b62f69e8d5ddb676cd29ad4b4a982f27c364f64c
Maximiliano Barria <maximiliano.barria@cloud.uautonoma.cl>
2025-05-19 15:29:02 -0400

    feat(user): agrega funcionalidad para crear usuario manualmente

--- backend/src/utils/hash.ts
+++ backend/src/utils/hash.ts
@@ -5,6 +5,6 @@ export const hashPassword = async (password: string): Promise<string> => {
   return await bcrypt.hash(password, saltRounds);
 };
 
-export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
-  return await bcrypt.compare(password, hashedPassword);
+export const comparePassword = async (password: string, hashed: string): Promise<boolean> => {
+  return await bcrypt.compare(password, hashed);
 };
